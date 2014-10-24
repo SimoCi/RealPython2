ef140b4e8af2b1b59a23485b38a39b48e7495b6c
simoneci <simonecidesk@gmail.com>
2014-10-24 18:39:15 +0200

    django bloggy

--- django-bloggy/bloggy_project/blog/admin.py
+++ django-bloggy/bloggy_project/blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from blog.models import Post
 
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'created_at')
+
 # Register your models here.
 # tell Django which models you want in the admin area
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
+
